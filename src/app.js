3a120b39e61ecba12edea656bc2a8eb8a2749985
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-10 21:15:09 +0530

    Learnt PATCH Method to update the student by id

--- src/app.js
+++ src/app.js
@@ -70,7 +70,46 @@ app.get("/students/:id", async (req, res) => {
 
         res.status(500).send(err);
     }
+});
+
+
+// Patch API (update the student by its id)
+app.patch("/students/:id", async (req, res) => {
+    // res.send("Patch API to update student detail by its _id");
+
+    try {
+        const _id = req.params.id;
+        const updateStudent = await Student.findByIdAndUpdate(_id, req.body, {new: true});
+
+        res.status(200).send(updateStudent);
+    } catch(e) {
+        console.log("Error: ", e);
+
+        res.status(404).send(updateStudent);
+    }
 })
+
+
+// Delete API (Delete single Student using _id)
+app.delete("/students/:id", async (req, res) => {
+    // res.send("Delete API to delete single student from database...");
+
+    try {
+        const _id = req.params.id;
+        console.log("Searched Student Id: "+_id);
+
+        const deletedData = await Student.findByIdAndDelete({_id});
+        console.log("->> "+deletedData);
+
+        if(!deletedData) {
+            return res.status(400).send();
+        } else {
+            res.status(200).send(deletedData);
+        }
+    } catch(err) {
+        res.status(500).send(err);
+    }
+});
 
 
 app.listen(port, (err) => {
